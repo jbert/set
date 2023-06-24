e420fc50f439840526deac88e1eb18b324919843
John Berthels <john.berthels@gmail.com>
2023-06-24 10:14:10 +0100

    split set implementation to own repo

---
+++ set.go
@@ -0,0 +1,114 @@
+package set
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Set[T comparable] map[T]struct{}
+
+func New[T comparable]() Set[T] {
+	return make(map[T]struct{})
+}
+
+func (s Set[T]) ForEach(f func(T)) {
+	for k, _ := range s {
+		f(k)
+	}
+}
+
+func (s Set[T]) String() string {
+	b := &strings.Builder{}
+	first := true
+	s.ForEach(func(e T) {
+		if first {
+			first = false
+		} else {
+			fmt.Fprintf(b, ", ")
+		}
+		fmt.Fprintf(b, "%v", e)
+	})
+	return b.String()
+}
+
+func Map[T, U comparable](s Set[T], f func(T) U) Set[U] {
+	m := New[U]()
+	s.ForEach(func(e T) {
+		m.Insert(f(e))
+	})
+	return m
+}
+
+func (s Set[T]) Contains(a T) bool {
+	_, ok := s[a]
+	return ok
+}
+
+func (s Set[T]) Intersect(a Set[T]) Set[T] {
+	i := New[T]()
+	for x, _ := range s {
+		if a.Contains(x) {
+			i.Insert(x)
+		}
+	}
+	return i
+}
+
+func (s Set[T]) Union(a Set[T]) Set[T] {
+	u := New[T]()
+	for x, _ := range s {
+		u.Insert(x)
+	}
+	for x, _ := range a {
+		u.Insert(x)
+	}
+	return u
+}
+
+func NewFromList[T comparable](as []T) Set[T] {
+	s := New[T]()
+	s.InsertList(as)
+	return s
+}
+
+func (s Set[T]) Equals(a Set[T]) bool {
+	if s.Size() != a.Size() {
+		return false
+	}
+	for x, _ := range s {
+		if !a.Contains(x) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s Set[T]) InsertList(as []T) {
+	for _, a := range as {
+		s.Insert(a)
+	}
+}
+
+func (s Set[T]) Insert(a T) {
+	s[a] = struct{}{}
+}
+
+func (s Set[T]) Remove(a T) {
+	delete(s, a)
+}
+
+func (s Set[T]) Size() int {
+	return len(s)
+}
+
+func (s Set[T]) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+func (s Set[T]) ToList() []T {
+	l := make([]T, 0)
+	for k, _ := range s {
+		l = append(l, k)
+	}
+	return l
+}
---
+++ set_test.go
@@ -0,0 +1,48 @@
+package set
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSet(t *testing.T) {
+	a := assert.New(t)
+	s := New[int]()
+
+	a.True(s.IsEmpty(), "Nil set is empty")
+
+	a.True(NewFromList([]int{1, 2, 3}).Equals(NewFromList([]int{3, 2, 1})), "order doesn't matter")
+
+	xs := []int{1, 3, 5, 8}
+	x := NewFromList(xs)
+	a.Equal(x.Size(), len(xs), "Set same length as list")
+	l := x.ToList()
+	sort.Ints(l)
+	a.Equal(l, xs, "ToList gives the right list")
+
+	x.InsertList(xs)
+	a.Equal(x.Size(), len(xs), "Inserting same list twice doesn't change set")
+
+	dups := []int{3, 5, 3, 7, 3, 11}
+	undups := []int{3, 5, 7, 11}
+	d := NewFromList(dups)
+	a.Equal(d.Size(), len(undups), "Sets don't contain duplicates")
+
+	ud := NewFromList(undups)
+	a.True(d.Equals(ud), "Set of dups and undups equal")
+
+	a.True(x.Equals(x), "Set equals itself")
+
+	u := s.Union(s)
+	a.True(s.Equals(u), "Self union doesn't change the set")
+
+	i := s.Intersect(s)
+	a.True(s.Equals(i), "Self intersect doesn't change the set")
+
+	ys := []int{3, 5, 7, -1, 100}
+	y := NewFromList(ys)
+	a.Equal(x.Intersect(y), NewFromList([]int{3, 5}), "intersect")
+	a.Equal(x.Union(y), NewFromList([]int{1, 3, 5, 7, 8, -1, 100}), "union")
+}
